573b45309b0822f1f959c1e84f7f37c4d2500afe
krak0ul <maxime.lorang@esme.fr>
2025-01-13 00:40:17 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys, os, time
+from datetime import datetime
+from hashlib import sha256
+
+def SHA256(text):
+    return sha256(text.encode("ascii")).hexdigest()
+
+def mine(block_number, transactions, previous_hash, prefix_zeros):
+# TODO
+    return
+
+
+
+def main():
+    return
+
+if __name__ == '__main__':
+    main()
